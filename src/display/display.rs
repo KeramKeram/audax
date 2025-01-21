6d9ada387a5ccb69d409c3dda35ccac93b21e287
KeramKeram <rev@autograf.pl>
2025-01-21 22:27:48 +0100

    Move const for board to display.rs

--- src/display/display.rs
+++ src/display/display.rs
@@ -1,5 +1,4 @@
 use super::tile::{Tile, TileType};
-use crate::{GRID_SIZE, SQUARE_SIZE};
 use macroquad::color::{BLACK, WHITE};
 use macroquad::prelude::{clear_background, draw_rectangle_lines, screen_height, screen_width};
 
@@ -15,6 +14,8 @@ pub struct Board {
 }
 
 impl Board {
+    const GRID_SIZE: usize = 10;
+    const SQUARE_SIZE: f32 = 50.0;
     pub fn new() -> Self {
         let screen_width = screen_width();
         let screen_height = screen_height();
@@ -24,17 +25,17 @@ impl Board {
     pub fn display(&self) {
         clear_background(WHITE);
 
-        let grid_width = GRID_SIZE as f32 * SQUARE_SIZE;
-        let grid_height = GRID_SIZE as f32 * SQUARE_SIZE;
+        let grid_width = Board::GRID_SIZE as f32 * Board::SQUARE_SIZE;
+        let grid_height = Board::GRID_SIZE as f32 * Board::SQUARE_SIZE;
         let offset_x = (self.screen_width - grid_width) / 2.0;
         let offset_y = (self.screen_height - grid_height) / 2.0;
 
-        for row in 0..GRID_SIZE {
-            for col in 0..GRID_SIZE {
-                let x = offset_x + col as f32 * SQUARE_SIZE;
-                let y = offset_y + row as f32 * SQUARE_SIZE;
+        for row in 0..Board::GRID_SIZE {
+            for col in 0..Board::GRID_SIZE {
+                let x = offset_x + col as f32 * Board::SQUARE_SIZE;
+                let y = offset_y + row as f32 * Board::SQUARE_SIZE;
 
-                draw_rectangle_lines(x, y, SQUARE_SIZE, SQUARE_SIZE, 2.0, BLACK);
+                draw_rectangle_lines(x, y, Board::SQUARE_SIZE, Board::SQUARE_SIZE, 2.0, BLACK);
             }
         }
     }
