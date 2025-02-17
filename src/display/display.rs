245b8c4c69681cb0eaa6725ba22883e9e95c9932
KeramKeram <rev@autograf.pl>
2025-02-17 22:42:37 +0100

    Add basic UI for battle screen.

--- src/display/display.rs
+++ src/display/display.rs
@@ -1,3 +1,4 @@
+use std::alloc::System;
 use super::tile::{Tile, TileType};
 use macroquad::color::{BLACK, WHITE};
 use macroquad::prelude::{clear_background, draw_rectangle_lines, screen_height, screen_width, load_texture, vec2, Texture2D, Image};
@@ -9,11 +10,21 @@ use macroquad::ui::{
 use std::fs::File;
 use std::io::Read;
 
+struct BattleIcons {
+    attack: Texture2D,
+    defend: Texture2D,
+    magic: Texture2D,
+    wait: Texture2D,
+    run: Texture2D,
+    negotiate: Texture2D,
+    system: Texture2D,
+}
+
 pub struct Board {
     screen_width: f32,
     screen_height: f32,
     pub tiles: Vec<Tile>,
-    texture2d: Texture2D,
+    battle_icons: BattleIcons,
 }
 
 fn load_texture_sync(path: &str) -> Texture2D {
@@ -32,12 +43,20 @@ impl Board {
         let tiles = vec![Tile::new(TileType::Empty); Self::GRID_SIZE * Self::GRID_SIZE];
         let screen_width = screen_width();
         let screen_height = screen_height();
-        let texture2d = load_texture_sync("button.png");
+        let battle_icons = BattleIcons {
+            attack: load_texture_sync("data/graphics/ui/battle/attack.png"),
+            defend: load_texture_sync("data/graphics/ui/battle/defence.png"),
+            magic: load_texture_sync("data/graphics/ui/battle/magic.png"),
+            wait: load_texture_sync("data/graphics/ui/battle/wait.png"),
+            run: load_texture_sync("data/graphics/general/hands.png"),
+            negotiate: load_texture_sync("data/graphics/general/hand.png"),
+            system: load_texture_sync("data/graphics/general/unit_defence.png"),
+        };
         Self {
             screen_width,
             screen_height,
             tiles,
-            texture2d
+            battle_icons
         }
     }
 
@@ -60,8 +79,53 @@ impl Board {
     }
 
     pub fn display_battle_interface(&self) {
-        if widgets::Button::new(self.texture2d.clone())
-            .size(vec2(100., 100.))
+        if widgets::Button::new(self.battle_icons.magic.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.attack.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.defend.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.wait.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.run.clone())
+            .size(vec2(80., 80.))
+            .position(vec2(self.screen_width - 80.0, 0.0))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.negotiate.clone())
+            .size(vec2(80., 80.))
+            .position(vec2(self.screen_width - 80.0, 80.0))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.system.clone())
+            .size(vec2(80., 80.))
+            .position(vec2(self.screen_width - 80.0, 160.0))
             .ui(&mut *root_ui())
         {
             println!("Textured button clicked!");
