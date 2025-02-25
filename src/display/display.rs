9c4b956af5909752c312f6395551215ff0307199
Keram <rev@autograf.pl>
2025-02-25 21:43:56 +0100

    Add battle UI Buttons (#4)

--- src/display/display.rs
+++ src/display/display.rs
@@ -1,16 +1,48 @@
+use std::alloc::System;
 use super::tile::{Tile, TileType};
 use macroquad::color::{BLACK, WHITE};
-use macroquad::prelude::{clear_background, draw_rectangle_lines};
+use macroquad::prelude::{clear_background, draw_rectangle_lines, screen_height, screen_width, load_texture, vec2, Texture2D, Image};
+use macroquad::ui::{
+    hash, root_ui,
+    widgets::{self, Group},
+    Drag, Ui,
+};
 use crate::common::display::WindowSize;
+
+use std::fs::File;
+use std::io::Read;
+
+struct BattleIcons {
+    attack: Texture2D,
+    defend: Texture2D,
+    magic: Texture2D,
+    wait: Texture2D,
+    run: Texture2D,
+    negotiate: Texture2D,
+    system: Texture2D,
+}
+
 
 pub struct Board {
     window_size: WindowSize,
     pub tiles: Vec<Tile>,
     grid_size: usize,
     square_size: f32,
+    battle_icons: BattleIcons,
+}
+
+fn load_texture_sync(path: &str) -> Texture2D {
+    let mut file = File::open(path).expect("Can't open file.");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("Read error.");
+
+    let image = Image::from_file_with_format(&buffer, None).unwrap();
+    Texture2D::from_image(&image)
 }
 
 impl Board {
+    const GRID_SIZE: usize = 10;
+    const SQUARE_SIZE: f32 = 50.0;
     pub fn new(width: f32, height: f32) -> Self {
         let grid_size = 12;
         let square_size = 50.0;
@@ -19,11 +51,21 @@ impl Board {
             screen_height: height,
         };
         let tiles = vec![Tile::new(TileType::Empty); grid_size * grid_size];
+        let battle_icons = BattleIcons {
+            attack: load_texture_sync("data/graphics/ui/battle/attack.png"),
+            defend: load_texture_sync("data/graphics/ui/battle/defence.png"),
+            magic: load_texture_sync("data/graphics/ui/battle/magic.png"),
+            wait: load_texture_sync("data/graphics/ui/battle/wait.png"),
+            run: load_texture_sync("data/graphics/general/hands.png"),
+            negotiate: load_texture_sync("data/graphics/general/hand.png"),
+            system: load_texture_sync("data/graphics/general/unit_defence.png"),
+        };
         Self {
             window_size,
             tiles,
             grid_size,
             square_size,
+            battle_icons
         }
     }
 
@@ -42,6 +84,60 @@ impl Board {
 
                 draw_rectangle_lines(x, y, self.square_size, self.square_size, 2.0, BLACK);
             }
+        }
+    }
+
+    pub fn display_battle_interface(&self) {
+        if widgets::Button::new(self.battle_icons.magic.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.attack.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.defend.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.wait.clone())
+            .size(vec2(80., 80.))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.run.clone())
+            .size(vec2(80., 80.))
+            .position(vec2(self.window_size.screen_width - 80.0, 0.0))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.negotiate.clone())
+            .size(vec2(80., 80.))
+            .position(vec2(self.window_size.screen_width - 80.0, 80.0))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
+        }
+
+        if widgets::Button::new(self.battle_icons.system.clone())
+            .size(vec2(80., 80.))
+            .position(vec2(self.window_size.screen_width - 80.0, 160.0))
+            .ui(&mut *root_ui())
+        {
+            println!("Textured button clicked!");
         }
     }
 
