e4811c9d9d1e49c3d2b67bd79d003a20c2f4e85c
KeramKeram <rev@autograf.pl>
2025-02-25 22:00:38 +0100

    Add mouse clicked event.

--- src/game/game_event.rs
+++ src/game/game_event.rs
@@ -2,4 +2,5 @@
 pub enum GameEvent {
     TileClicked,
     WindowResized,
+    MouseCliked
 }
--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ async fn main() {
 
     let mut board = display::Board::new(screen_width, screen_height);
     let (tx, rx) = mpsc::channel();
-    let handler = Arc::new(crate::game::MouseClickHandler {});
+    let handler_mouse_cliked = Arc::new(crate::game::MouseClickHandler {});
     let handler_window_size = Arc::new(crate::game::WindowResizeHandler {});
     let loop_thread = std::thread::spawn(move || {
         let event_loop = crate::game::EventLoop::new(rx, vec![]);
-        event_loop.register_handler(GameEvent::TileClicked, handler.clone());
+        event_loop.register_handler(GameEvent::MouseCliked, handler_mouse_cliked.clone());
         event_loop.register_handler(GameEvent::WindowResized, handler_window_size.clone());
         event_loop.start();
     });
@@ -29,9 +29,7 @@ async fn main() {
         if is_mouse_button_pressed(MouseButton::Left) {
             let (mouse_x, mouse_y) = mouse_position();
             let my_vec: Vec<u8> = vec![10, 20];
-            //event_loop.add_event(game_event::GameEvent::TileClicked {
-            //}, my_vec);
-            tx.send((GameEvent::TileClicked, my_vec)).unwrap();
+            tx.send((GameEvent::MouseCliked, my_vec)).unwrap();
             print!("Mouse clicked at ({}, {})\n", mouse_x, mouse_y);
         }
 
