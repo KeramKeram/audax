66101134b0cd5b50e1efe68d8a65e72aa42fbb9a
KeramKeram <rev@autograf.pl>
2025-03-18 21:52:44 +0100

    Add unit to tile

--- src/display/tile.rs
+++ src/display/tile.rs
@@ -1,6 +1,9 @@
 use crate::common::display::texture::load_texture_sync;
 use macroquad::prelude::Texture2D;
 
+#[derive(Debug, Clone)]
+struct Unit;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum TileType {
     Empty,
@@ -15,6 +18,7 @@ pub struct Tile {
     pub tile_type: TileType,
     texture: Texture2D,
     pub back_light: bool,
+    unit: Option<Unit>
 }
 
 impl Tile {
@@ -22,7 +26,14 @@ impl Tile {
         Self {
             tile_type: tile_type,
             texture: load_texture_sync(texture_path),
-            back_light: false
+            back_light: false,
+            unit: None,
         }
     }
+
+    pub fn set_unit(&mut self, unit: Unit, tile_type: TileType) {
+        self.unit = Some(unit);
+    }
 }
+
+
